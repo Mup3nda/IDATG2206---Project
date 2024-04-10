1ef8e53967a789fb546c70c8fec49b0fd84fac4a
Mup3nda <mupendadidier@gmail.com>
2024-04-10 12:06:17 +0200

    fjernet unødvendige filer

--- test_image.py
+++ test_image.py
@@ -4,7 +4,7 @@ import torch
 
 model = YOLO('runs/detect/train2/weights/last.pt')
 #model = YOLO('yolov8m.pt')
-results = model("/Users/didiermupenda/ANADROM_Datasets/Annotation/task_fish_detection-2024_02_02_06_13_45-cvat for video 1.1/images/frame_000092.PNG", show=True)
+results = model("/Users/didiermupenda/ANADROM_Datasets/Annotation/task_fish_detection-2024_02_05_18_52_05-cvat for video 1.1/all_data10/frame_000016_d10.PNG", show=True)
 print(results)
 
 cv2.waitKey(0)
